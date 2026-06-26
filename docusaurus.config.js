65fb299fa908d31cb8141073f955520db97092d9
Bartek <58663723+morbidreich@users.noreply.github.com>
2026-06-26 20:49:56 +0200

    Revert "update base url"
    This reverts commit 2b86894bd23898ca1360e10b00651fbb7398953b.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://wiki.plvacc.pl',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/welcome',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
