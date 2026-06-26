8bd80afb0e7d619fadb0b8635635138904d0f1b1
Bartek <58663723+morbidreich@users.noreply.github.com>
2026-06-26 21:41:28 +0200

    idk#2

--- docs/welcome.md
+++ docs/welcome.md
@@ -1,5 +1,6 @@
 ---
 sidebar_position: 1
+slug: /
 ---
 
 # Witaj na wiki PLVACC
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,13 +19,6 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-                  <Link
-                    className="button button--secondary button--lg"
-                    to="/docs/welcome">
-                    Dokumentacja
-                  </Link>
-                </div>
       </div>
     </header>
   );
