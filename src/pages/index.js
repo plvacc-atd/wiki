20d96e3c9330e1d73128bc09d84f93f47e24f159
Bartek <58663723+morbidreich@users.noreply.github.com>
2026-06-26 21:34:38 +0200

    idk

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
+import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Heading from '@theme/Heading';
 import Layout from '@theme/Layout';
+import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading';
