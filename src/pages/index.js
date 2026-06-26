a253d6429d2c913fed6838bb9350ce9f6baed9a4
Bartek <58663723+morbidreich@users.noreply.github.com>
2026-06-26 21:30:03 +0200

    add button to wiki

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,13 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+                  <Link
+                    className="button button--secondary button--lg"
+                    to="/docs/welcome">
+                    Dokumentacja
+                  </Link>
+                </div>
       </div>
     </header>
   );
